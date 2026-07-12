3c552c70e320b7198b14c473c93471b75eb709b9
hosseinorajzade <139715363+hosseinorajzade@users.noreply.github.com>
2026-07-12 16:24:26 +0330

    add init_func.go

---
+++ basics/init_func.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("initializtion process 1....")
+}
+func init() {
+	fmt.Println("initializtion process 2....")
+}
+func init() {
+	fmt.Println("initializtion process 3....")
+}
+func main() {
+	fmt.Println("main func")
+
+}
